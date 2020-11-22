eca80213d8a661323c4edaa3477482f219b505e3
richelbilderbeek <richel@richelbilderbeek.nl>
2020-11-22 13:39:40 +0100

    Fix rJava error

--- push_all_githubs.sh
+++ push_all_githubs.sh
@@ -9,7 +9,7 @@ do
   then
     echo "git folder: "$folder
     git add --all :/
-    git commit -m "Do not install imagemagic PPA"
+    git commit -m "Fix rJava error"
     git pull
     git push
   fi
