8347c72090f067af7e06b7dae24ed8d28ca64194
richelbilderbeek <richel@richelbilderbeek.nl>
2020-12-20 16:50:11 +0100

    EOD

--- push_all_githubs.sh
+++ push_all_githubs.sh
@@ -9,7 +9,7 @@ do
   then
     echo "git folder: "$folder
     git add --all :/
-    git commit -m "Moved PhD thesis to github.com/richelbilderbeek/phd_thesis (from github.com/richelbilderbeek/thesis)"
+    git commit -m "EOD"
     git pull
     git push
   fi
