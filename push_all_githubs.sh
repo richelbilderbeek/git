5c9062ef13f4c05bb039e8e16309653acf641ef4
richelbilderbeek <richel@richelbilderbeek.nl>
2021-01-03 20:00:56 +0100

    EOD

--- push_all_githubs.sh
+++ push_all_githubs.sh
@@ -9,7 +9,7 @@ do
   then
     echo "git folder: "$folder
     git add --all :/
-    git commit -m "Use subshell (instead of backtick) and a more precise regex"
+    git commit -m "EOD"
     git pull
     git push
   fi
