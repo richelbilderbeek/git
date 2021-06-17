1101179c0f1e0e1798a98a7091e66191363fbba8
richelbilderbeek <richel@richelbilderbeek.nl>
2021-06-17 15:17:59 +0200

    Cannot do 'ggplot2::ggplot(...) + ggplot2::ggsave(...)', do 'ggplot2::ggplot(...); ggplot2::ggsave(...)' instead, see https://github.com/tidyverse/ggplot2/issues/4517

--- push_all_githubs.sh
+++ push_all_githubs.sh
@@ -9,7 +9,7 @@ do
   then
     echo "git folder: "$folder
     git add --all :/
-    git commit -m "EOD"
+    git commit -m "Cannot do 'ggplot2::ggplot(...) + ggplot2::ggsave(...)', do 'ggplot2::ggplot(...); ggplot2::ggsave(...)' instead, see https://github.com/tidyverse/ggplot2/issues/4517"
     git pull
     git push
   fi
