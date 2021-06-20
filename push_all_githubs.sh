7d33383018195fe525fbd3c8b064a62b4234b0fc
richelbilderbeek <richel@richelbilderbeek.nl>
2021-06-20 10:16:23 +0200

    Use 'https://travis-ci.com' (instead of obsolete 'https://travis-ci.org')

--- push_all_githubs.sh
+++ push_all_githubs.sh
@@ -9,7 +9,7 @@ do
   then
     echo "git folder: "$folder
     git add --all :/
-    git commit -m "EOD"
+    git commit -m "Use 'https://travis-ci.com' (instead of obsolete 'https://travis-ci.org')"
     git pull
     git push
   fi
