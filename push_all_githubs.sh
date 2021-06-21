8c07dc416cb5afb22042faf72c4de66c9dfc7b9a
richelbilderbeek <richel@richelbilderbeek.nl>
2021-06-21 10:32:00 +0200

    EOD

--- push_all_githubs.sh
+++ push_all_githubs.sh
@@ -9,7 +9,7 @@ do
   then
     echo "git folder: "$folder
     git add --all :/
-    git commit -m "Use 'https://travis-ci.com' (instead of obsolete 'https://travis-ci.org')"
+    git commit -m "EOD"
     git pull
     git push
   fi
