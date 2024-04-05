498d949e2ada1e9a6675596a87a945905c7671ff
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-04-05 11:30:16 +0200

    EOD

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-git config --global user.email "richel@richelbilderbeek.nl"
+git config --global user.email "rjcbilderbeek@gmail.com"
 git config --global user.name "richelbilderbeek"
 git config --global alias.praise blame
 
