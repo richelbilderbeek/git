f3bfc69efc27301df0b306cee690b537904d46a4
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-07-12 10:51:41 +0200

    Update chapters

--- setup.sh
+++ setup.sh
@@ -2,6 +2,10 @@ git config --global user.email "rjcbilderbeek@gmail.com"
 git config --global user.name "richelbilderbeek"
 git config --global alias.praise blame
 
+# Merge by default upon a pull
+git config --global pull.rebase false
+
 # Password every ten hours
 git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=36000'
+
